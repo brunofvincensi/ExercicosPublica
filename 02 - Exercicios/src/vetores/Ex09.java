b20f94f7ff52c95317b3e850d16440ca43d7e5ef
Isadorass <viniciusgabriel.com@gmail.com>
2022-03-09 15:24:55 -0300

    finalizado

--- 02 - Exercicios/src/vetores/Ex09.java
+++ 02 - Exercicios/src/vetores/Ex09.java
@@ -6,9 +6,68 @@ public class Ex09 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+				
+		String[] palavras = {"cachorro", "xadrez", "zebra", "calculadora"};
+		
+		int indice;		
+	
+		do {
+		indice =  (int) (Math.random() * 10);
+		}while(indice >= 4);
 		
 		
+		System.out.println(indice);
 		
+		char[] palavra = new char[palavras[indice].length()];
+		
+		char[] palavraCompleta = new char[palavras[indice].length()];
+		
+		for(int i = 0; i < palavraCompleta.length; i++) {
+			
+			palavraCompleta[i] = palavras[indice].charAt(i);			
+		}
+		
+		boolean palavraAchada = false;
+		
+		System.out.println("A palavra tem "+ palavraCompleta.length + " letras");
+		
+		
+        for(int i = 0; i < palavra.length; i++) {
+			
+			palavra[i] = '?';
+		}
+			
+		while(!palavraAchada) {
+		
+		for(char letra : palavra) {
+			
+			System.out.print(letra + " ");
+		}
+		System.out.println();
+			
+		System.out.println("Adicione uma letra");
+		char letra = sc.next().charAt(0);
+		 	
+		for(int i = 0; i < palavraCompleta.length; i++) {
+			
+			if(palavraCompleta[i] == letra) {
+				palavra[i] = letra;
+			}
+		}
+			
+		palavraAchada = true;	
+
+		// valida se a palavra foi encontrada
+       for(int i = 0; i < palavra.length; i++) {
+			
+			if(palavra[i] == '?' ) {
+				palavraAchada = false;
+				break;
+			}
+		  }
+		}
+		
+		System.out.println("Parabens voce achou! a palavra é " + palavras[indice]);
 		
 		sc.close();
 
--- 02 - Exercicios/src/vetores/Ex14.java
+++ 02 - Exercicios/src/vetores/Ex14.java
@@ -7,9 +7,90 @@ public class Ex14 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		String[][] quiz = new String[20][6];
+		String[][] quiz = {
+			
+			{"Quanto é 6 x 5? ", "56", "23", "30", "36", "3"},
+			{"Qual é a capital do Brasil? ", "Santa Catarina", "Brasilia", "Sao Paulo", "Rio de Janeiro", "2"},
+			{"De quem é a famosa frase “Penso, logo existo”?", "Platão", "Galileu Galilei", "Descartes", "Sócrates", "4"},
+			{"Qual o livro mais vendido no mundo a seguir à Bíblia? ", "O Senhor dos Anéis", "Dom Quixote", "O Pequeno Príncipe", "Ela, a Feiticeira", "2"},
+			{"Quantas casas decimais tem o número pi? ", "Duas", "Centenas", "Infinitas", "Milhares", "3"},
+			{"O que a palavra legend significa em português? ", "Legenda", "Conto", "História", "Lenda", "4"},
+			{"Quem pintou Guernica? ", "Paul Cézanne", "Pablo Picasso", "Diego Rivera", "Tarsila do Amaral", "2"},
+			{"Qual a nacionalidade de Che Guevara? ", "Argentina", "Boliviana", "Panamenha", "Peruana", "1"},
+		};
+				
+		String[][] usuarios = new String[20][2];
 		
+		char jogarNovamente = 'y';
+		int indice;
+		int cont = 0;
 		
+		while(jogarNovamente == 'y') {
+						
+			System.out.println("Insira seu nome: ");
+			String nome = sc.next();
+			
+			String[][] quizSelecionado = new String[4][6];
+			int acertos = 0;
+			
+			for(int i = 0; i < quizSelecionado.length; i++) {
+				
+				do {
+					 indice = (int) Math.floor(Math.random() * 10);
+					}while(indice >= 8);
+				
+				
+				boolean existe = false;
+				
+				for(int j = 0; j < i; j++) {
+					
+				if(quiz[indice][0].equals(quizSelecionado[j][0])) {
+					existe = true;
+					i--;
+					break;		
+				  }
+				}
+				if(!existe) {
+					quizSelecionado[i] = quiz[indice];
+				}
+				
+			}
+						
+			for(String[] pergunta : quizSelecionado) {
+				System.out.println(pergunta[0]);
+				System.out.println();
+				System.out.println("1) " + pergunta[1]);
+				System.out.println("2) " + pergunta[2]);
+				System.out.println("3) " + pergunta[3]);
+				System.out.println("4) " + pergunta[4]);
+				
+				String resposta = sc.next();
+				
+				if(resposta.equals(pergunta[5])) {
+					acertos++;
+				}
+				
+			}
+			
+			usuarios[cont][0] = nome;
+			usuarios[cont][1] = Integer.toString(acertos);
+		
+			cont++;
+			System.out.println("Jogar novamente? y(yes) n(no)");
+			jogarNovamente = sc.next().charAt(0);
+			
+		}
+		
+		for(String[] usuario : usuarios) {
+			
+			if(usuario[0] != null) {
+			
+			System.out.println();
+			System.out.print("Nome: " + usuario[0]);
+			System.out.print("  Acertos: " + usuario[1]);
+			}
+		}
+					
 		sc.close();
 	}
 
